59f492f71921db9cfb5ff8252458fe9612d92dec
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2022-03-16 22:58:31 +0100

    zmiana przełączania stron

--- index.php
+++ index.php
@@ -6,15 +6,13 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" type="text/css" href="css/style.css" />
-    <script src="scripts/jquery.js"></script>
-    <script src="scripts/reload.js"></script>
 </head>
 
 <body>
     <div id="all">
         <div id="header">
             <div id="logo">
-                <a href="#" data-target="php/home" class="logolink">
+                <a href="index.php" class="logolink">
                     <img src="pictures/logo.png" alt="bloody hand" />
                     <span class="logotekst1">MOVIEHUB</span>
                 </a>
@@ -22,12 +20,12 @@
         </div>
         <div id="navigation">
             <ol>
-                <li><a href="#" data-target="php/home">Strona Główna</a></li>
-                <li><a href="#" data-target="php/ranking">Ranking</a></li>
+                <li><a href="index.php">Strona Główna</a></li>
+                <li><a href="php/ranking.php">Ranking</a></li>
                 <li>
                     <input type="text" placeholder="Wpisz aby wyszukać film" id="search">
                 </li>
-                <li><a href="#" data-target="php/login">Logowanie</a></li>
+                <li><a href="php/login.php">Logowanie</a></li>
             </ol>
         </div>
         <div id="content">
--- php/signup.php
+++ php/signup.php
@@ -1,4 +1,3 @@
-
 <?php
 require("signup-script.php");
 ?>
@@ -9,92 +8,27 @@ require("signup-script.php");
     <title>Moviehub</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-
-    <style>
-        #div_login {
-            width: 290px;
-            height: 270px;
-            margin: 0 auto;
-        }
-
-        #div_login h1 {
-            margin-top: 0px;
-            font-weight: normal;
-            padding: 10px;
-            text-align: center;
-            color: white;
-            font-family: 'Josefin Sans', sans-serif;
-        }
-
-        #div_login div {
-            clear: both;
-            margin-top: 10px;
-            padding: 5px;
-        }
-
-        #div_login .textbox {
-            width: 96%;
-            padding: 7px;
-        }
-
-        #div_login input[type=submit] {
-            padding: 6px;
-            width: 102.5%;
-            background-color: #e52d27;
-            border: 0px;
-            color: white;
-            font-size: 20px;
-        }
-
-        #div_login textarea {
-            width: 96%;
-            padding: 7px;
-        }
-
-        a.sign {
-            padding: 6px;
-            font-size: 20px;
-            width: 100px;
-            color: white;
-            text-decoration: none;
-            background-color: #e52d27;
-            text-align: center;
-        }
-
-        .sign:hover {
-            background-color: #f63e38;
-        }
-
-        input[type=password] {
-            display: block;
-            height: 38px;
-            border: none;
-            width: 280px;
-            text-align: center;
-        }
-
-    </style>
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
 </head>
 
 <body>
     <div id="all">
         <div id="header">
             <div id="logo">
-                <a href="#" class="logolink">
-                    <img src="pictures/logo.png" alt="bloody hand" />
+                <a href="../index.php" class="logolink">
+                    <img src="../pictures/logo.png" alt="bloody hand" />
                     <span class="logotekst1">MOVIEHUB</span>
                 </a>
             </div>
         </div>
         <div id="navigation">
             <ol>
-                <li><a href="#">Strona Główna</a></li>
-                <li><a href="#">Ranking</a></li>
+                <li><a href="../index.php">Strona Główna</a></li>
+                <li><a href="../php/ranking.php">Ranking</a></li>
                 <li>
                     <input type="text" placeholder="Wpisz aby wyszukać film" id="search">
                 </li>
-                <li><a href="#">Logowanie</a></li>
+                <li><a href="../php/login.php">Logowanie</a></li>
             </ol>
         </div>
         <div id="content">
@@ -117,7 +51,7 @@ require("signup-script.php");
                         <input type="submit" value="Zarejestruj" name="but_submit" id="but_submit" />
                     </div>
                     <div>
-                        <a href="#" data-target="php/login" class="sign">Powrót do Logowania</a>
+                        <a href="login.php" class="sign">Powrót do Logowania</a>
                     </div>
                     <div>
                         <?php
@@ -152,105 +86,3 @@ require("signup-script.php");
 </body>
 
 </html>
-=======
-<style>
-    #div_login {
-        width: 290px;
-        height: 270px;
-        margin: 0 auto;
-    }
-
-    #div_login h1 {
-        margin-top: 0px;
-        font-weight: normal;
-        padding: 10px;
-        text-align: center;
-        color: white;
-        font-family: 'Josefin Sans', sans-serif;
-    }
-
-    #div_login div {
-        clear: both;
-        margin-top: 10px;
-        padding: 5px;
-    }
-
-    #div_login .textbox {
-        width: 96%;
-        padding: 7px;
-    }
-
-    #div_login input[type=submit] {
-        padding: 6px;
-        width: 102.5%;
-        background-color: #e52d27;
-        border: 0px;
-        color: white;
-        font-size: 20px;
-    }
-
-    #div_login textarea {
-        width: 96%;
-        padding: 7px;
-    }
-
-    a.sign {
-        padding: 6px;
-        font-size: 20px;
-        width: 100px;
-        color: white;
-        text-decoration: none;
-        background-color: #e52d27;
-        text-align: center;
-    }
-
-    .sign:hover {
-        background-color: #f63e38;
-    }
-    input[type=password] {
-    display: block;
-    height: 38px;
-    border: none;
-    width: 280px;
-    text-align: center;
-}
-</style>
-<script>
-    $(document).ready(function() {
-        var trigger = $('a'),
-            container = $('#content');
-        trigger.on('click', function() {
-            var $this = $(this)
-            target = $this.data('target');
-            container.load(target + '.php');
-            return false;
-        });
-    });
-
-</script>
-<form method="post">
-    <div id="div_login">
-        <h1>Rejestracja</h1>
-        <div>
-            <input type="text" class="textbox" id="txt_uname" name="txt_uname" placeholder="Nazwa użytkownika" />
-        </div>
-        <div>
-            <input type="text" class="textbox" id="txt_email" name="txt_email" placeholder="E-mail" />
-        </div>
-        <div>
-            <input type="password" class="textbox" id="txt_pwd" name="txt_pwd" placeholder="Hasło" />
-        </div>
-        <div>
-            <input type="password" class="textbox" id="txt_confirmpwd" name="txt_confirmpwd" placeholder="Powtórz hasło" />
-        </div>
-        <div>
-            <input type="submit" value="Zarejestruj" name="but_submit" id="but_submit" />
-        </div>
-        <div>
-            <a href="#" data-target="php/login" class="sign">Powrót do Logowania</a>
-        </div>
-        <div>
-        </div>
-    </div>
-</form>
-
