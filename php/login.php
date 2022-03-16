55bbe5d951e184af60205e777de202aaf5d3691a
Marcin Piasek <83632941+marcinpiasek128@users.noreply.github.com>
2022-03-16 23:14:31 +0100

    naprawiony css

--- php/login.php
+++ php/login.php
@@ -9,6 +9,7 @@ require_once("login-script.php");
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <link rel="stylesheet" type="text/css" href="../css/style.css" />
+    <link rel="stylesheet" type="text/css" href="../css/login.css" />
 </head>
 
 <body>
